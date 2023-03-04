a90dd9725d1842d04076dd2a3446a3f9bd98638d
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 14:37:12 -0300

    feat: allows to order the repository list by query argument

--- app/Http/Controllers/GetGithubUserController.php
+++ app/Http/Controllers/GetGithubUserController.php
@@ -6,13 +6,21 @@ use App\Models\GithubUser;
 use App\Models\GithubUserRepository;
 use Exception;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class GetGithubUserController extends Controller
 {
     public function handle(Request $request, $githubUser)
     {
-        try {
+        try 
+        {
+            $repository_order = $request->query('repository_order') ?? 'desc';
+
+            if (!in_array($repository_order, ['asc', 'desc'])) {
+                throw new BadRequestHttpException('repository_order');
+            }
+
             $requestGithubUserController = new RequestGithubUserController();
             $requestGithubUserRepositoriesController = new RequestGithubUserRepositoriesController();
 
@@ -26,6 +34,16 @@ class GetGithubUserController extends Controller
                     ,'stars' => $repository['stargazers_count']
                     ,'url' => $repository['html_url']
                 ]));
+            }
+
+            if ($repository_order == 'desc') {
+                usort($userRepositories, function ($a, $b) {
+                    return $a['stars'] < $b['stars'];
+                });
+            } else {
+                usort($userRepositories, function ($a, $b) {
+                    return $a['stars'] > $b['stars'];
+                });
             }
 
             $githubUser = new GithubUser([
@@ -45,6 +63,9 @@ class GetGithubUserController extends Controller
                 case 404:
                     $message = 'Usuário não encontrado!';
                     break;
+                case 400:
+                    $message = "O campo $message não foi alimentado com um valor válido.";
+                    break; 
             }
             return response()->json(['message' => $message], $e->getStatusCode(), array(), JSON_UNESCAPED_SLASHES);
         } catch (Exception $e) {
