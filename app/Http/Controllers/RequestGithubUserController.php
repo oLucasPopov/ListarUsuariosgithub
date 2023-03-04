5f64c9184bb77d1b8c6916509655d8680c701c6c
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 13:49:29 -0300

    feat: RequestGithubUsercontroller throws NotFoundHttpException when the github APi returns 404

--- app/Http/Controllers/RequestGithubUserController.php
+++ app/Http/Controllers/RequestGithubUserController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RequestGithubUserController extends Controller
 {
-    public function handle($user){
+    public function handle($user)
+    {
         $response = Http::get("https://api.github.com/users/$user");
+
+        if ($response->status() == 404 ) {
+            throw new NotFoundHttpException();
+        }
+
         return $response->json();
     }
 }
