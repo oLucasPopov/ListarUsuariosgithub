4ef7b74740caad9fb541dcfa590845a4dc4d1fb3
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 13:54:55 -0300

    feat: RequestGithubUserController throws if github returns status 403

--- app/Http/Controllers/RequestGithubUserController.php
+++ app/Http/Controllers/RequestGithubUserController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class RequestGithubUserController extends Controller
 {
     public function handle($user)
     {
+        $url = "https://api.github.com/users/$user";
         $response = Http::get("https://api.github.com/users/$user");
 
-        if ($response->status() == 404 ) {
+        if ($response->status() == 404) {
             throw new NotFoundHttpException();
+        }
+
+        if ($response->status() == 403) {
+            throw new UnauthorizedHttpException(
+                $url,
+                'Quantidade de requisições excedida, tente novamente mais tarde!'
+            );
         }
 
         return $response->json();
