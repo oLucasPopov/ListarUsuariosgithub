a1d70eec0cea6f6916e294961bb20979a5270f71
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 16:47:50 -0300

    feat: create GetGithubRepositoryController returning the requested data

--- app/Http/Controllers/GetGithubRepositoryController.php
+++ app/Http/Controllers/GetGithubRepositoryController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Adapters\Github\GithubRepositoryJsonAdapter;
+use App\Helpers\ControllerHelper;
+use App\Http\Controllers\GithubAPI\RequestGithubRepositoryController;
+use Exception;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class GetGithubRepositoryController extends Controller
 {
-    public function handle(){
-        
+    public function handle($user, $repository)
+    {
+        try {
+            $requestGithubRepositoryController = new RequestGithubRepositoryController();
+            $githubRepositoryResponse = $requestGithubRepositoryController->handle($user, $repository);
+            $githubRepository = GithubRepositoryJsonAdapter::adapt($githubRepositoryResponse);
+
+            return ControllerHelper::return_response($githubRepository);
+        } catch (Exception | HttpException $e) {
+            return ControllerHelper::return_error($e);
+        }
     }
 }
