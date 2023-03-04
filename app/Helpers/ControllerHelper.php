0ae26f562aaf77044772c0814edcf6bc0e782b48
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 15:16:46 -0300

    refactor: move response return to a helper function

---
+++ app/Helpers/ControllerHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Helpers;
+
+use Exception;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ControllerHelper
+{
+    public static function return_response($data = null, $statusCode = 200)
+    {
+        return response()->json($data, $statusCode, array(), JSON_UNESCAPED_SLASHES);
+    }
+
+    public static function return_error(Exception | HttpException $e) {
+      $message = $e->getMessage();
+      $statusCode = 500;
+      if ($e instanceof HttpException) {
+        $statusCode = $e->getStatusCode();
+        switch ($e->getStatusCode()) {
+            case 404:
+                $message = 'Usuário não encontrado!';
+                break;
+            case 400:
+                $message = "O campo $message não foi alimentado com um valor válido.";
+                break; 
+        }
+
+        return response()->json(['message' => $message], $e->getStatusCode(), array(), JSON_UNESCAPED_SLASHES);
+      } else {
+        $message = 'Ocorreu um erro interno: ' . $e->getMessage();
+        $statusCode = 500;
+      }
+      return response()->json(['message'=> $message], $statusCode);
+
+    }
+}
--- app/Http/Controllers/GetGithubUserController.php
+++ app/Http/Controllers/GetGithubUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ControllerHelper;
 use App\Http\Controllers\GithubAPI\RequestGithubUserController;
 use App\Http\Controllers\GithubAPI\RequestGithubUserRepositoriesController;
 use App\Models\GithubUser;
@@ -58,20 +59,9 @@ class GetGithubUserController extends Controller
                 'repositories' => $userRepositories
             ]);
 
-            return response()->json($githubUser, 200, array(), JSON_UNESCAPED_SLASHES);
-        } catch (HttpException $e) {
-            $message = $e->getMessage();
-            switch ($e->getStatusCode()) {
-                case 404:
-                    $message = 'Usuário não encontrado!';
-                    break;
-                case 400:
-                    $message = "O campo $message não foi alimentado com um valor válido.";
-                    break; 
-            }
-            return response()->json(['message' => $message], $e->getStatusCode(), array(), JSON_UNESCAPED_SLASHES);
-        } catch (Exception $e) {
-            return response()->json(['message'=>'Ocorreu um erro interno: ' . $e->getMessage()], 500);
-        }
+            return ControllerHelper::return_response($githubUser);
+        } catch (Exception | HttpException $e) {
+            return ControllerHelper::return_error($e);
+        } 
     }
 }
