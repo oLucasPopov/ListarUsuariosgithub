0b7aa29eaae6f3e354f9a4ac2e053bdeb49b8b4f
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 13:26:23 -0300

    feat: returns github user controller infos

--- app/Http/Controllers/GetGithubUserController.php
+++ app/Http/Controllers/GetGithubUserController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GithubUser;
 use Illuminate\Http\Request;
 
 class GetGithubUserController extends Controller
 {
-    //
+    public function handle(Request $request, $githubUser)
+    {
+        $requestGithubUserController = new RequestGithubUserController();
+        $githubUserResponse = $requestGithubUserController->handle($githubUser);
+
+        $githubUser = new GithubUser([
+            'id' => $githubUserResponse['id'],
+            'followers_count' => $githubUserResponse['followers'],
+            'following_count' => $githubUserResponse['following'],
+            'avatar_image' => $githubUserResponse['avatar_url'],
+            'email' => $githubUserResponse['email'],
+            'bio' => $githubUserResponse['bio']
+        ]);
+
+        return response()->json($githubUser, 200, array(), JSON_UNESCAPED_SLASHES);
+    }
 }
