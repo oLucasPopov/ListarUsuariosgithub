4c3ad44b5b491035ceed2a65d49410e28d5998a5
lucaspopov@protonmail.com <oLucasPopov>
2023-03-04 16:24:15 -0300

    feat: create GetGithubRepository Controller

---
+++ app/Http/Controllers/GetGithubRepositoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GetGithubRepositoryController extends Controller
+{
+    public function handle(){
+        
+    }
+}
