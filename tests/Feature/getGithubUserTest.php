7c673123f4ca8dee5ff5fa09b1d103c4873cfbea
lucaspopov@protonmail.com <oLucasPopov>
2023-03-05 10:39:16 -0300

    test: ensure getGithubUser route returns 200 when descending ordering is provided in lowercase

--- tests/Feature/getGithubUserTest.php
+++ tests/Feature/getGithubUserTest.php
@@ -26,6 +26,12 @@ class getGithubUserTest extends TestCase
         $response->assertStatus(200);
     }    
 
+    public function test_getGithubUser_route_should_return_200_if_descending_order_in_lowercase_is_provided(): void
+    {
+        $response = $this->get('/api/getGithubUser/olucaspopov?repository_order=desc');
+        $response->assertStatus(200);
+    }
+
     public function test_getGithubUser_route_should_return_404_if_user_is_not_found(): void
     {
         // O usuário admin é proibido pelo Github
