af7ff512925d9e656e2f2deaaa8fd531d62656ae
lucaspopov@protonmail.com <oLucasPopov>
2023-03-05 08:43:40 -0300

    test: ensure ArrayHelper::sort_array sorts ascending correctly when equal values are provided in array

--- tests/Unit/ArrayHelperTest.php
+++ tests/Unit/ArrayHelperTest.php
@@ -36,7 +36,31 @@ class ArrayHelperTest extends TestCase
         $lastObjID = -1;
         foreach ($fake_objects as $fake_object) {
             $CurrentObjID = $fake_object['id'];
-            if ($CurrentObjID <= $lastObjID) {
+            if ($CurrentObjID < $lastObjID) {
+                $this->fail("O Objeto atual é menor que o objeto anterior!  $CurrentObjID < $lastObjID ");
+            }
+            $lastObjID = $CurrentObjID;
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function test_sort_array_should_sort_asc_correctly_when_equal_values_are_provided(): void
+    {
+        $fake_objects = array(
+            new FakeObject(['id' => 1, 'email' => "joao@example.com"]),
+            new FakeObject(['id' => 1, 'email' => "maria@example.com"]),
+            new FakeObject(['id' => 2, 'email' => "pedro@example.com"]),
+            new FakeObject(['id' => 2, 'email' => "ana@example.com"]),
+            new FakeObject(['id' => 4, 'email' => "carlos@example.com"])
+        );
+
+        ArrayHelper::sort_array($fake_objects, 'id', 'asc');
+
+        $lastObjID = -1;
+        foreach ($fake_objects as $fake_object) {
+            $CurrentObjID = $fake_object['id'];
+            if ($CurrentObjID < $lastObjID) {
                 $this->fail("O Objeto atual é menor que o objeto anterior!  $CurrentObjID < $lastObjID ");
             }
             $lastObjID = $CurrentObjID;
@@ -54,7 +78,7 @@ class ArrayHelperTest extends TestCase
         $lastObjID = null;
         foreach ($fake_objects as $fake_object) {
             $CurrentObjID = $fake_object['id'];
-            if (($lastObjID !== null) && ($CurrentObjID >= $lastObjID)) {
+            if (($lastObjID !== null) && ($CurrentObjID > $lastObjID)) {
                 $this->fail("O Objeto atual é maior que o objeto anterior!  $CurrentObjID > $lastObjID ");
             }
             $lastObjID = $CurrentObjID;
@@ -72,7 +96,7 @@ class ArrayHelperTest extends TestCase
         $lastObjID = null;
         foreach ($fake_objects as $fake_object) {
             $CurrentObjID = $fake_object['id'];
-            if (($lastObjID !== null) && ($CurrentObjID >= $lastObjID)) {
+            if (($lastObjID !== null) && ($CurrentObjID > $lastObjID)) {
                 $this->fail("O Objeto atual é maior que o objeto anterior!  $CurrentObjID > $lastObjID ");
             }
             $lastObjID = $CurrentObjID;
@@ -97,12 +121,12 @@ class ArrayHelperTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function test_sort_array_should_throw_if_wrong_key_is_provided(): void {
+    public function test_sort_array_should_throw_if_wrong_key_is_provided(): void
+    {
         $fake_objects = $this->mock_unordered_array();
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('O Valor passado para a chave a ser ordenada está incorreto!');
 
         ArrayHelper::sort_array($fake_objects, 'WRONG_KEY', 'ASC');
-
     }
 }
