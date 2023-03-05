ad9f3f78105761c25b8fd51c90a6d5bf0c0642f3
lucaspopov@protonmail.com <oLucasPopov>
2023-03-05 00:31:21 -0300

    feat: ArrayHelper::sort_array throws if an invalid sort order is provided

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -6,8 +6,14 @@ class ArrayHelper
 {
   public static function sort_array(array &$array, string $array_sortable_field, $sort)
   {
-    if (strtoupper($sort) == 'DESC') {
-      usort($array, function ($a, $b) use ($array_sortable_field){
+    $sort_order = strtoupper($sort);
+
+    if (!in_array($sort_order, ['ASC', 'DESC'])) {
+      throw new \InvalidArgumentException('O Valor passado para o tipo de ordenação está incorreto!');
+    }
+
+    if ($sort_order == 'DESC') {
+      usort($array, function ($a, $b) use ($array_sortable_field) {
         return $a[$array_sortable_field] < $b[$array_sortable_field];
       });
     } else {
