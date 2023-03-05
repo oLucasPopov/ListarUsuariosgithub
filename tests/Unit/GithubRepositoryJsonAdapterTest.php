a4c28572c5b8474bbe19e8181e209143a678bfca
lucaspopov@protonmail.com <oLucasPopov>
2023-03-05 09:07:00 -0300

    test: ensure GithubRepositoryJsonAdapter::adapt returns a github repository with the correct values when exceding fields are provided

--- tests/Unit/GithubRepositoryJsonAdapterTest.php
+++ tests/Unit/GithubRepositoryJsonAdapterTest.php
@@ -8,6 +8,17 @@ use PHPUnit\Framework\TestCase;
 
 class GithubRepositoryJsonAdapterTest extends TestCase
 {
+    private function mockGithubRepository(): GithubRepository
+    {
+        return new GithubRepository([
+            'name' => 'test',
+            'description' => 'test',
+            'stars' => 1,
+            'language' => 'test',
+            'github_url' => 'test'
+        ]);
+    }
+
     function test_adapt_should_return_a_github_repository_with_correct_values(): void
     {
         $json = [
@@ -18,15 +29,27 @@ class GithubRepositoryJsonAdapterTest extends TestCase
             'html_url' => 'test'
         ];
 
-        $expectedGithubRepository = new GithubRepository([
+        $expectedGithubRepository = $this->mockGithubRepository();
+
+        $returnedGithubRepository = GithubRepositoryJsonAdapter::adapt($json);
+
+        $this->assertEquals($expectedGithubRepository, $returnedGithubRepository);
+    }
+
+    function test_adapt_should_return_a_github_repository_with_correct_values_when_exceding_fields_are_provided(): void
+    {
+        $json = [
             'name' => 'test',
             'description' => 'test',
-            'stars' => 1,
+            'stargazers_count' => 1,
             'language' => 'test',
-            'github_url' => 'test'
-        ]);
+            'html_url' => 'test',
+            'exceding_key' => 'exceding_value'
+        ];
 
-        $returnedGithubRepository = GithubRepositoryJsonAdapter::adapt($json); 
+        $expectedGithubRepository = $this->mockGithubRepository();
+
+        $returnedGithubRepository = GithubRepositoryJsonAdapter::adapt($json);
 
         $this->assertEquals($expectedGithubRepository, $returnedGithubRepository);
     }
