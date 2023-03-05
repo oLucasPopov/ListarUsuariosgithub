136678232ed878a770b0f867eca7394a825661dc
lucaspopov@protonmail.com <oLucasPopov>
2023-03-05 00:18:42 -0300

    fix: fixed changes on function $sort parameter case differences

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -6,7 +6,7 @@ class ArrayHelper
 {
   public static function sort_array(array &$array, string $array_sortable_field, $sort)
   {
-    if ($sort == 'desc') {
+    if (strtoupper($sort) == 'DESC') {
       usort($array, function ($a, $b) use ($array_sortable_field){
         return $a[$array_sortable_field] < $b[$array_sortable_field];
       });
